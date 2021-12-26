a7f629ede52ed6a4ffc03f26ab8d3d0674cbf583
nazibulislam <nazibulislam2013@gmail.com>
2021-12-27 00:51:56 +0600

    Closed scanner

--- src/AreaOfHexagon.java
+++ src/AreaOfHexagon.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.sql.rowset.spi.SyncResolver;
-
 public class AreaOfHexagon {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
@@ -9,6 +7,8 @@ public class AreaOfHexagon {
     short length = sc.nextShort();
     System.out.println("Area = " + (6*Math.pow(length,2)/(4*Math.tan(Math.PI/6))));
     System.out.println(Math.toRadians(57.3));
+
+    sc.close();
   }
   
 }
