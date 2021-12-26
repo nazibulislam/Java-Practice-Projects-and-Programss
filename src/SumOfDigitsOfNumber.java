3f1cab48a163eec0c6593cbc59a345214890c4cb
nazibulislam <nazibulislam2013@gmail.com>
2021-12-26 22:55:58 +0600

    Completed Sub of digits of number program

--- src/SumOfDigitsOfNumber.java
+++ src/SumOfDigitsOfNumber.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class SumOfDigitsOfNumber {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    
+    System.out.println("Enter a number to sum its digits:");
+    int number = sc.nextInt();
+    int result = 0;
+    while(number != 0){
+      result += number%10;
+      number /= 10;
+    }
+    System.err.println("Result = " + result);
+    sc.close();
   }
 }
