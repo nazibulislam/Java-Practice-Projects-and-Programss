c4db6cfc03fefde43bc34df6a6cc00f1b5cc8ddc
nazibulislam <nazibulislam2013@gmail.com>
2021-12-29 00:16:07 +0600

    Completed Find ACSII Value program

---
+++ src/FindASCIIValue.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FindASCIIValue {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a character:");
+    char c = sc.next().charAt(0);
+    System.out.println("The ASCII value of '" + c + "' is = " + (int)c);
+  }
+}
